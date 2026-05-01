c7eec695f998be5a2488d6b6264cbcf5090cde87
nashalexisdolores <alxsdlrs@gmail.com>
2026-05-01 19:02:58 +0800

    define PascalCase CalculatorBase and operations mapping

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,1 +1,11 @@
 import sys
+
+class CalculatorBase:
+    """Base class for shared calculator properties."""
+    def __init__(self):
+        self.operations_menu = {
+            '1': 'Addition',
+            '2': 'Subtraction',
+            '3': 'Multiplication',
+            '4': 'Division'
+        }
